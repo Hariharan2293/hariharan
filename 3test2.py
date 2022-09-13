b9ee4cb4ea6d896c231f154710c9fc843759c083
Hariharan2293 <112700316+Hariharan2293@users.noreply.github.com>
2022-09-13 10:53:25 +0530

    Add files via upload

---
+++ 3test2.py
@@ -0,0 +1,32 @@
+n=int(input("Enter a number:"))
+if(n>0 and n<=999):
+    a=int(n/100)
+    b=int(n%100)
+    c=int(b/10)
+    d=int(b%10)
+    print(a,c,d)
+    print(d,c,a)
+    print(c,d,a)
+    print(c,a,d)
+    print(d,a,c)
+    print(a,d,c)
+if(n>0 and n>=1000):
+    o=int(n/1000)
+    p=int(n%1000)
+    q=int(p/100)
+    r=int(p%100)
+    s=int(r/10)
+    t=int(r%10)
+    print(o,q,s,t)
+    print(t,s,q,o)
+    print(q,s,o,t)
+    print(t,s,o,q)
+    print(s,q,t,o)
+    print(t,o,s,q)
+    print(s,o,q,t)
+if(n<0):
+    print("POSITIVE NUMBER REQUIRED!")
+
+    
+    
+    
