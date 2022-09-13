991cd3317c0c98de183d514bb94104baff096dad
Hariharan2293 <112700316+Hariharan2293@users.noreply.github.com>
2022-09-13 10:57:51 +0530

    Update 3test2.py

--- 3test2.py
+++ 3test2.py
@@ -10,23 +10,7 @@ if(n>0 and n<=999):
     print(c,a,d)
     print(d,a,c)
     print(a,d,c)
-if(n>0 and n>=1000):
-    o=int(n/1000)
-    p=int(n%1000)
-    q=int(p/100)
-    r=int(p%100)
-    s=int(r/10)
-    t=int(r%10)
-    print(o,q,s,t)
-    print(t,s,q,o)
-    print(q,s,o,t)
-    print(t,s,o,q)
-    print(s,q,t,o)
-    print(t,o,s,q)
-    print(s,o,q,t)
 if(n<0):
     print("POSITIVE NUMBER REQUIRED!")
-
-    
-    
-    
+if(n>=1000):
+    print("ENTERED NUMBER IS BIGGER THAN EXPECTED!")
