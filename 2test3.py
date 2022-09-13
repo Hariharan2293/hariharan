204f53f5057458176e5213e3049c846edaf2f465
Hariharan2293 <112700316+Hariharan2293@users.noreply.github.com>
2022-09-13 09:19:15 +0530

    Add files via upload

---
+++ 2test3.py
@@ -0,0 +1,11 @@
+b= int(input("Enter length of array:"))
+a=[]
+e=[]
+for i in range (0,b):
+    c=input ("Enter the string") 
+    a.append(c)
+for i in range (0,b):
+    d=len (a[i].split())
+    e.append(d)
+print("LIST=",a)
+print("MAX words in a string =",max(e))
--- 2test4.py
+++ 2test4.py
@@ -1,8 +1,37 @@
-a =input("Enter:")
-b =input("Enter:")
-lst=[]
-lst.extend(a)
-lst.extend(b)
-lst.sort()
-print(lst)
+try:
 
+a=int (input("Enter number of elements in first list:"))
+
+b=[ ]
+
+for i in range (0,a):
+
+c=int (input("Enter element"))
+
+b.append(c)
+
+d=int(input("Enter number of elements in second list:"))
+
+e=[ ]
+
+for i in range (0,d):
+
+f=int(input("Enter element"))
+
+e.append(f)
+
+g=[ ]
+
+g=b+e
+
+g.sort()
+
+print("LIST1 -",b)
+
+print ("LIST2 -",e)
+
+print("Merged and sorted list -",g)
+
+except ValueError:
+
+print()
